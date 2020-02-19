55f28ae6dbdc6d957bdfd2d01d114e68fe66dbbc
hansa-lallu <hansalallu@gmail.com>
2020-02-19 21:32:35 +0000

    thermostat has a default of 20 degrees

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,22 +1,6 @@
-function Player() {
-}
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
+'use strict';
+
+function Thermostat() {
+  this._temperature = 20
 };
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
-
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
