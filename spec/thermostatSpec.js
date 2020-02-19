277b2b98af3312c08afafd2254ecf92dc6c2bb74
hansa-lallu <hansalallu@gmail.com>
2020-02-19 22:29:24 +0000

    energy status

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -74,4 +74,31 @@ describe('Thermostat', function(){
       expect(thermostat.currentTemp()).toEqual(32);
     });
   });
+
+  describe('displaying usage levels', function() {
+    describe('when the temp is below 18 degrees', function() {
+      it('is considered low-usage', function() {
+        for (var i = 0; i < 3; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+  });
+
+  describe('when the temp is between 18 and 25 degrees', function() {
+    it('it is considered medium-usage', function() {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+  });
+
+  describe('when the temp is anything else', function() {
+    it('it is considered high-usage', function() {
+      thermostat.powerSavingMode = false;
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -7,6 +7,7 @@ function Thermostat() {
   this.powerSavingMode = true;
   this.MAX_TEMP_PSM_ON = 25;
   this.MAX_TEMP_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 };
 
 Thermostat.prototype.currentTemp = function(){
@@ -53,3 +54,13 @@ Thermostat.prototype.isMaximumTemperature = function() {
 Thermostat.prototype.resetTemp = function(){
   this._temperature = this.DEFAULT_TEMP;
 };
+
+Thermostat.prototype.energyUsage = function() {
+  if (this._temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this._temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this._temperature <= this.MAX_TEMP_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+}
