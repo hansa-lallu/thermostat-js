b45a9c8514d8237cae8bd628d7b6713a7005fb04
hansa-lallu <hansalallu@gmail.com>
2020-02-21 11:49:42 +0000

    able to enter a city and retrieve temp

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,7 @@ var thermostat = new Thermostat();
 $(document).ready(function() {
       $('.current_temp').text(thermostat._temperature + '˚C');
       $('.energy_usage').text(thermostat.energyUsage());
+      displayWeather();
     });
 
     $('.temp_up').click(function() {
@@ -34,8 +35,18 @@ $(document).ready(function() {
       $('.energy_usage').text(thermostat.energyUsage());
       $('body').attr('class', thermostat.energyUsage());
     }
+    
+    function displayWeather(city) {
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+      var units =  '&units=metric';
+      $.get(url + token + units, function(data) {
+        $('#outside-temp').text(data.main.temp + '˚C');
+      });
+    };
 
-    $.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric", function(data) {
-    $('#outside-temp').text(data.main.temp + '˚C')
-    });
-
+    $('#select-city').submit(function(event){
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+      });
